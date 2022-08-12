be1c524fe563f35de5b74096bf1736fd31c4f7f2
SarvarVakhobov <sarvarbek0125@gmail.com>
2022-08-12 21:33:00 +0500

    admin page added

--- education/admin.py
+++ education/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Teacher)
+admin.site.register(Timetable)
+admin.site.register(Branch)
+admin.site.register(Group)
+admin.site.register(Course)
+admin.site.register(LevelOfgroup)
+admin.site.register(Lesson)
+admin.site.register(Student)
+admin.site.register(Room)
--- exam/admin.py
+++ exam/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Marking)
+admin.site.register(Marking_system)
+admin.site.register(Exams)
+admin.site.register(Exams_list)
